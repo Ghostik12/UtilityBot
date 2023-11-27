c05916914114b30ee4a2a7f8dc8f4567f01ba3ec
Vsevolod Luzhaev <luzaevvsevolod@gmail.com>
2023-11-27 23:16:37 +0300

    UtilityBot

---
+++ UtilityBot/UtilityBot/Bot.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Exceptions;
+
+namespace UtilityBot
+{
+    internal class Bot : BackgroundService
+    {
+        private ITelegramBotClient _telegramBotClient;
+
+        public Bot(ITelegramBotClient telegramBotClient)
+        {
+            _telegramBotClient = telegramBotClient;
+        }
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            _telegramBotClient.StartReceiving(HandleUpdateAsync, HandleErrorAsync, new ReceiverOptions() { AllowedUpdates = { } }, cancellationToken: stoppingToken);
+            Console.WriteLine("Bot Launch");
+            return;
+        }
+
+        async Task HandleUpdateAsync (ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        {
+            if (update.Type == UpdateType.Message)
+                switch (update.Message!.Type) 
+                {
+                    case MessageType.Text :
+                        Console.WriteLine($"{update.Message.Text}");
+                        string str = update.Message.Text;
+                        await _telegramBotClient.SendTextMessageAsync(update.Message.Chat.Id, $"Длина сообщения: {str.Length} знаков", cancellationToken: cancellationToken);
+                        return;
+                    default:
+                        await _telegramBotClient.SendTextMessageAsync(update.Message.Chat.Id, $"Не верный формат сообщения, отправте сообщение", cancellationToken: cancellationToken);
+                        return;
+                }
+
+            if (update.Type == UpdateType.CallbackQuery)
+                await _telegramBotClient.SendTextMessageAsync(update.CallbackQuery.From.Id, "Не верный формат сообщения, отправте сообщение", cancellationToken : cancellationToken);
+            return;
+        }
+
+        Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+        {
+            var errorMessage = exception switch
+            {
+                ApiRequestException apiRequestException
+                    => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+                _ => exception.ToString()
+            };
+
+            Console.WriteLine(errorMessage);
+
+            Console.WriteLine("Ожидаем 10 секунд перед повторным подключением.");
+            Thread.Sleep(10000);
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ UtilityBot/UtilityBot/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Text;
+using Telegram.Bot;
+
+namespace UtilityBot
+{
+    class Program
+    {
+        public static async Task Main()
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+
+            var host = new HostBuilder()
+                .ConfigureServices((hostContext, services) => ConfigureServices(services))
+                .UseConsoleLifetime()
+                .Build();
+
+            Console.WriteLine("Launch");
+
+            await host.RunAsync();
+            Console.WriteLine("Stop");
+        }
+
+        static void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<ITelegramBotClient>(provider => new TelegramBotClient("6511541259:AAFl-SdhJFFLxDs2m9nBEbmuu0oHcPUyFUs"));
+
+            services.AddHostedService<Bot>();
+        }
+    }
+}
